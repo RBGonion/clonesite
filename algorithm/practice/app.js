e7e40f3d115c39c6c4aea8af33a0786b8c314fca
RBGonion <yopi27@naver.com>
2022-04-21 22:43:29 +0900

    4.21.study

--- algorithm/practice/app.js
+++ algorithm/practice/app.js
@@ -2,10 +2,55 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let count = Number(input[0]);
-let numbers = input[1].split(" ").map((x) => Number(x));
+const entireTestCase = parseInt(input[0]);
 
-numbers.sort((a, b) => a - b);
+solution();
 
-console.log(`${numbers[0]} ${numbers[count - 1]}`);
-console.log(numbers);
+function solution() {
+  for (let i = 1; i < input.length; i++) {
+    let inputArray = input[i].split(" ").map((x) => +x);
+    let partTestCase = inputArray[0];
+    let addNumber = 0;
+    let everageRecord;
+    let count = 0;
+    let SuperiorEverage = 0;
+
+    for (let j = 1; j < inputArray.length; j++) {
+      addNumber += inputArray[j];
+      everageRecord = addNumber / (inputArray.length - 1);
+    }
+
+    for (let k = 1; k < inputArray.length; k++) {
+      if (inputArray[k] > everageRecord) {
+        count++;
+      }
+      SuperiorEverage = count / (inputArray.length - 1);
+    }
+    console.log((SuperiorEverage * 100).toFixed(3) + "%");
+  }
+}
+/*
+let entireTestCase = Number(input[0]);
+
+for (let i = 1; i <= entireTestCase; i++) {
+  let cases = input[i].split(" ");
+  let scoreCaseCount = Number(cases[0]);
+  let sum = 0;
+  console.log(scoreCaseCount);
+
+  for (let j = 1; j <= scoreCaseCount; j++) {
+    sum += Number(cases[j]);
+  }
+  let reverage = sum / scoreCaseCount;
+
+  let highScoreStudent = 0;
+  for (let k = 1; k <= scoreCaseCount; k++) {
+    if (reverage < cases[k]) {
+      highScoreStudent++;
+    }
+  }
+
+  result = ((highScoreStudent / scoreCaseCount) * 100).toFixed(3);
+  console.log(result + "%");
+}
+*/
--- codingApple/menubar/main.js
+++ codingApple/menubar/main.js
@@ -1,11 +1,10 @@
 const Xbutton = document.querySelector("header span:last-child");
-
 const idnavdisplay = document.querySelector("#idnav");
-
 const targetElm = document.getElementById("idnav");
-
 const targetDisplay =
   targetElm && window.getComputedStyle(targetElm).getPropertyValue("display");
+
+Xbutton.addEventListener("click", navAppear);
 
 function navAppear() {
   if (targetDisplay === "flex" || "") {
@@ -14,4 +13,3 @@ function navAppear() {
     idnavdisplay.style.display = "flex";
   }
 }
-Xbutton.addEventListener("click", navAppear);

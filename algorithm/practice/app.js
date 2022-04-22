8f8b2bf2f9ec83fa7f10222791fc9846370c0d96
RBGonion <yopi27@naver.com>
2022-04-22 11:21:42 +0900

    4.22study

--- algorithm/practice/app.js
+++ algorithm/practice/app.js
@@ -2,55 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-const entireTestCase = parseInt(input[0]);
+let entireArray = [];
+let constructorArray = [];
+let nonConstArray = [];
 
-solution();
+for (let n = 1; n <= 10000; n++) {
+  entireArray.push(n);
+  const StringN = String(n);
+  let temporaryN = n;
 
-function solution() {
-  for (let i = 1; i < input.length; i++) {
-    let inputArray = input[i].split(" ").map((x) => +x);
-    let partTestCase = inputArray[0];
-    let addNumber = 0;
-    let everageRecord;
-    let count = 0;
-    let SuperiorEverage = 0;
+  for (let i = 0; i < StringN.length; i++) {
+    temporaryN += Number(StringN[i]); // n + Number(String[0] + Number(String[1] + Number(String[2]... 값을 구함
+  }
+  constructorArray.push(temporaryN); // constructorArray에 중복인 값을 제외하고 넣어야함 그리고 배열에서 1~10000 중 없는 수를 찾아야 한다
 
-    for (let j = 1; j < inputArray.length; j++) {
-      addNumber += inputArray[j];
-      everageRecord = addNumber / (inputArray.length - 1);
-    }
-
-    for (let k = 1; k < inputArray.length; k++) {
-      if (inputArray[k] > everageRecord) {
-        count++;
-      }
-      SuperiorEverage = count / (inputArray.length - 1);
-    }
-    console.log((SuperiorEverage * 100).toFixed(3) + "%");
+  if (constructorArray.indexOf(entireArray[n - 1]) === -1) {
+    nonConstArray.push(n);
   }
 }
-/*
-let entireTestCase = Number(input[0]);
 
-for (let i = 1; i <= entireTestCase; i++) {
-  let cases = input[i].split(" ");
-  let scoreCaseCount = Number(cases[0]);
-  let sum = 0;
-  console.log(scoreCaseCount);
-
-  for (let j = 1; j <= scoreCaseCount; j++) {
-    sum += Number(cases[j]);
-  }
-  let reverage = sum / scoreCaseCount;
-
-  let highScoreStudent = 0;
-  for (let k = 1; k <= scoreCaseCount; k++) {
-    if (reverage < cases[k]) {
-      highScoreStudent++;
-    }
-  }
-
-  result = ((highScoreStudent / scoreCaseCount) * 100).toFixed(3);
-  console.log(result + "%");
+for (let i = 0; i < nonConstArray.length; i++) {
+  console.log(nonConstArray[i]);
 }
-*/
+//지금 해야되는건 생성자가 있는 수랑 없는 수를 찾는 것이다

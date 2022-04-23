56b3fa7cd6457f97cac8f7eb600624a69b97e40e
RBGonion <yopi27@naver.com>
2022-04-23 16:23:41 +0900

    4.23study

--- algorithm/practice/app.js
+++ algorithm/practice/app.js
@@ -1,27 +1,3 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
-
-let entireArray = [];
-let constructorArray = [];
-let nonConstArray = [];
-
-for (let n = 1; n <= 10000; n++) {
-  entireArray.push(n);
-  const StringN = String(n);
-  let temporaryN = n;
-
-  for (let i = 0; i < StringN.length; i++) {
-    temporaryN += Number(StringN[i]); // n + Number(String[0] + Number(String[1] + Number(String[2]... 값을 구함
-  }
-  constructorArray.push(temporaryN); // constructorArray에 중복인 값을 제외하고 넣어야함 그리고 배열에서 1~10000 중 없는 수를 찾아야 한다
-
-  if (constructorArray.indexOf(entireArray[n - 1]) === -1) {
-    nonConstArray.push(n);
-  }
-}
-
-for (let i = 0; i < nonConstArray.length; i++) {
-  console.log(nonConstArray[i]);
-}
-//지금 해야되는건 생성자가 있는 수랑 없는 수를 찾는 것이다
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
--- zerochoJS/답지.js
+++ zerochoJS/답지.js
@@ -1,13 +1,17 @@
 var 단어 = document.createElement("div");
 단어.innerText = "제로초";
 document.body.append(단어);
+
 var 폼 = document.createElement("form");
 document.body.append(폼);
+
 var 입력창 = document.createElement("input");
 폼.append(입력창);
+
 var 버튼 = document.createElement("button");
 버튼.textContent = "입력!";
 폼.append(버튼);
+
 var 결과창 = document.createElement("div");
 document.body.append(결과창);
 

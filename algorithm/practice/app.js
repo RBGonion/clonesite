48d621f6674a9e0efdb5cef07563ccd7358f64e7
RBGonion <yopi27@naver.com>
2022-04-27 21:21:08 +0900

    4.27study

--- algorithm/practice/app.js
+++ algorithm/practice/app.js
@@ -2,79 +2,14 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const testCase = +input[0];
-let tureFalseBox = [];
-let count = 0;
-
-for (let i = 1; i < input.length; i++) {
-  let splitInput = input[i].split("");
-  let tempArr = [];
-
-  for (let j = 0; j < splitInput.length; j++) {
-    if (j === 0) {
-      tempArr.push(splitInput[j]);
-      continue;
-    }
-    if (0 < j < splitInput.length - 1) {
-      if (tempArr.indexOf(splitInput[j]) === -1) {
-        tempArr.push(splitInput[j]);
-        continue;
-      } else {
-        if (
-          splitInput[j] === splitInput[j - 1] ||
-          splitInput[j] === splitInput[j - 1]
-        ) {
-          tempArr.push(splitInput[j]);
-          continue;
-        }
-      }
-    }
-
-    if (j === splitInput.length - 1) {
-      if (tempArr.indexOf(splitInput[j]) === -1) {
-        tempArr.push(splitInput[j]);
-        continue;
-      }
-    }
-  }
-  tureFalseBox.push("false");
-}
-console.log(testCase - tureFalseBox.length);
-
 /*
-const testCase = +input[0];
-let count = 0;
+input = +input;
 
-for (let i = 1; i < input.length; i++) {
-  const splitInput = input[i].split("");
-  let tempArr = [];
-  let tureFalseBox = [];
-
-  for (let j = 0; j < splitInput.length; j++) {
-    if (j === 0) {
-      tempArr.push(splitInput[j]);
-    } else if (0 < j < splitInput.length - 1) {
-      if (
-        tempArr.indexOf(splitInput[j]) === -1 &&
-        (splitInput[j] === splitInput[j - 1] ||
-          splitInput[j] === splitInput[j + 1])
-      ) {
-        tempArr.push(splitInput[j]);
-      } else {
-        tureFalseBox.push("false");
-      }
-    } else if (
-      j === splitInput.length - 1 &&
-      tempArr.indexOf(splitInput[j]) !== -1 &&
-      splitInput[j - 1] !== splitInput[j - 2]
-    ) {
-      tureFalseBox.push("false");
-    }
+for (let i = 0; i < input; i++) {
+  let answer = "";
+  for (let j = 0; j <= i; j++) {
+    answer += "*";
   }
-
-  if (tureFalseBox.length === 0) {
-    count++;
-  }
+  console.log(answer);
 }
-console.log(count);
 */

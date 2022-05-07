0ad78768492b310a434dbf9467b414113d4a76a0
RBGonion <yopi27@naver.com>
2022-05-07 20:53:02 +0900

    5.7study

--- algorithm/practice/app.js
+++ algorithm/practice/app.js
@@ -1,10 +1,3 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-input = input[0];
-
-if (input < 10) {
-  input = "0" + String(input);
-}
-console.log(input);
